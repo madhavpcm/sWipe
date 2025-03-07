e582e09112c09af7ae426281ca6161ebde37937d
Amalkrishna A S <amalkrishna02016@gmail.com>
2025-03-07 21:52:55 +0530

    monthlist schema updated

--- components/ongoing-list/index.tsx
+++ components/ongoing-list/index.tsx
@@ -104,18 +104,18 @@ export const OngoingList = () => {
     
     return (
         <View className="flex-1 px-5 mt-12">
-            <View className="flex flex-row justify-between  ">
+            <View className="flex flex-row justify-between border-b-2  border-gray-200 border-dashed pb-1">
                 <Title text="Ongoing" subtitle="Pickup where you left off" />
             </View>
             <View
-                className="mt-3 h-full pb-12"
+                className="mt-3 h-[400px]a"
             >
                 <FlatList
                     className=" "
                     data={data}
                     renderItem={renderItem}
                     keyExtractor={(item, index) => index.toString()}
-                    contentContainerStyle={{ paddingBottom: 20 }}
+                    contentContainerStyle={{ paddingBottom: 175 }}
                 />
             </View>
 
