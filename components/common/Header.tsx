0ccfca88a8d46d99047d0d4f0ebd16feecbda057
Amalkrishna A S <amalkrishna02016@gmail.com>
2025-03-07 19:13:56 +0530

    banner redesign , more routes , start month screen

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,34 +1,50 @@
 import { View, Text, TouchableOpacity, StatusBar } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons,MaterialIcons } from "@expo/vector-icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import React from "react";
+import { router, usePathname } from "expo-router";
 
 export default function Header() {
+  const pathname = usePathname()
 
 
-  const routestoShow = [
-    ''
+  const tabRoutes= [
+    '/album',
+    '/month',
+    '/'
   ]
   const insets = useSafeAreaInsets();
 
   return (
     <View
-      className="h-24 bg-white flex justify-between px-6 flex-row"
+      className="h-20 bg-white pt-3 flex justify-between px-6 flex-row"
     >
+      <TouchableOpacity
+      onPress={()=> router.back()}
+      
+      >
+      {!tabRoutes.includes(pathname)&&<MaterialIcons
+      name="chevron-left"
+      size={25}
+      color={'#1e293b'}
+      />}
+      </TouchableOpacity>
       <Text
         className="text-4xl font-rubik-semibold text-blue-600"
       >
         sWipe
       </Text>
-      <TouchableOpacity
+      {<TouchableOpacity
+      onPress={()=>router.navigate('/(root)/settings')}
       
       >
-      <Ionicons
-      name="settings-sharp"
+      <MaterialIcons
+      name="settings"
       size={25}
       color={'#1e293b'}
       />
       </TouchableOpacity>
+       }
    
 
     </View>
